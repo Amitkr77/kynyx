61f3a5fbd4158645b37cbe665ed1c4099490a880
Amitkr77 <amitroyk99@gmail.com>
2025-07-30 16:17:18 +0530

    added components to home page of the website

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,18 +1,24 @@
 import React from "react";
 
-
-
-
 import Hero from "./components/Hero";
 import Features from "./components/Features";
+import Services from "./components/Services";
+import Approach from "./components/Approach";
+import PortfolioSection from "./components/PortfolioSection";
+import Testimonials from "./components/Testimonials";
+import TechStack from "./components/TechStack";
+import WhyChooseUs from "./components/WhyChooseUs";
 
 const Home = () => {
   return (
     <div className="font-['Inter'] bg-[#0d0d1a] text-white overflow-x-hidden">
-      
       <Hero />
-      <Features />
-      
+      <Services />
+      <Approach />
+      <PortfolioSection />
+      <Testimonials />
+      <TechStack />
+      <WhyChooseUs />
     </div>
   );
 };
