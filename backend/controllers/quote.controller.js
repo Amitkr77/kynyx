c543bf8a5dd35f52dde74deba97bbf6e476793af
Abhinav Anand <erabhipc@gmail.com>
2025-08-01 13:28:20 +0530

    Added saveQuoteToGoogleSheet feature

--- backend/controllers/quote.controller.js
+++ backend/controllers/quote.controller.js
@@ -1,4 +1,5 @@
 const sendQuoteEmail = require("../services/quoteMail.service");
+const saveQuoteToGoogleSheet = require("../services/saveQuoteToGoogleSheet");
 const sanitize = require("../utils/sanitize");
 
 const handleQuote = async (req, res, next) => {
@@ -9,6 +10,7 @@ const handleQuote = async (req, res, next) => {
     });
 
     await sendQuoteEmail(data);
+    await saveQuoteToGoogleSheet(data);
     res.status(200).json({ message: "Quote sent successfully!" });
   } catch (error) {
     next(error);
